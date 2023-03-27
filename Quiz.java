74338184fe7c790ef757bb7304c4dfa5c88dce7c
gbedward123 <64324618+gbedward123@users.noreply.github.com>
2023-03-26 21:00:16 -0400

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,15 @@
+public class Quiz implements Measurable {
+    private double score;
+
+    public Quiz(double score) {
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public double getMeasure() {
+        return score;
+    }
+}
